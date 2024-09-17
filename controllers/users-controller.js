8e7ce77b517b76e7c06251820cec16b615055c82
francids <francisco.mc.ids@gmail.com>
2024-09-17 09:04:34 -0400

    feat: Add getUserById function to users controller

--- controllers/users-controller.js
+++ controllers/users-controller.js
@@ -3,3 +3,12 @@ import users from "../data/users.js";
 export function getUsers(req, res) {
   res.json(users);
 }
+
+export function getUserById(req, res) {
+  const user = users.find((user) => user.id === parseInt(req.params.id));
+  if (!user) {
+    res.status(404).json({ message: `User with id ${req.params.id} not found` });
+  } else {
+    res.json(user);
+  }
+}
--- routes/router.js
+++ routes/router.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import { getRoot, getPong } from '../controllers/main-controller.js';
-import { getUsers } from '../controllers/users-controller.js';
+import { getUsers, getUserById } from '../controllers/users-controller.js';
 
 const router = express.Router();
 
 router.get('/', getRoot);
 router.get('/ping', getPong);
 router.get("/users", getUsers);
+router.get("/users/:id", getUserById);
 
 export default router;
