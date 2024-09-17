637f740f187fb1e6adcb97f4f8a9b0423bb7a2d7
francids <francisco.mc.ids@gmail.com>
2024-09-17 00:33:56 -0400

    refactor: Update import paths for main and users controllers

--- routes/router.js
+++ routes/router.js
@@ -1,6 +1,6 @@
 import express from 'express';
-import { getRoot, getPong } from '../controllers/mainController.js';
-import { getUsers } from '../controllers/usersController.js';
+import { getRoot, getPong } from '../controllers/main-controller.js';
+import { getUsers } from '../controllers/users-controller.js';
 
 const router = express.Router();
 
