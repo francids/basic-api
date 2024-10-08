2fbaee386924c9fdf82cf078a2bfec2cc5a6b563
francids <francisco.mc.ids@gmail.com>
2024-10-07 21:23:11 -0400

    refactor: Update users controller to handle GET requests with query parameters

--- controllers/users.ts
+++ controllers/users.ts
@@ -4,9 +4,12 @@ import usersData from "../data/users";
 
 export const users = new Hono().basePath("/users");
 
-users.get("/", (c) => c.json(usersData));
-users.get("/:id", (c) => {
-  const id = parseInt(c.req.param("id"));
-  const user = usersData.find((user) => user.id === id);
-  return user ? c.json(user) : c.json({ msg: `User with id ${id} not found` }, 404);
+users.get("/", (c) => {
+  const { id } = c.req.query();
+
+  if (id) {
+    const user = usersData.find((user) => user.id === parseInt(id));
+    return user ? c.json(user) : c.json({ msg: `User with id ${id} not found` }, 404);
+  }
+  return c.json(usersData);
 });
